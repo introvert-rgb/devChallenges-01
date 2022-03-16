7c269253670fb02d297492f720fa67976916e1a9
Anup Singh <anups5246@gmail.com>
2022-03-16 13:01:59 +0530

    text

--- App/App.js
+++ App/App.js
@@ -1,9 +1,12 @@
 import template from './template.js';
+
 export default class WebPage extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = template.render();
+        this.shadowRoot.querySelector('.img').setAttribute('src', '../Scarecrow.png');
+
     }
 }
 if (!customElements.get('error-webpage')) {
